f2bb95b41ccb51b186b0c33130dabb224b028f1f
juzero98 <illo00@naver.com>
2020-03-23 21:54:46 +0900

    Login, MediaStore, Upload to Storage

---
+++ app/src/main/java/com/example/imagemanageapp/LoginActivity.kt
@@ -0,0 +1,61 @@
+package com.example.imagemanageapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        doLogin()
+    }
+
+    // Loading 화면으로 전환
+    fun startLoading() {
+        val intent = Intent(baseContext, LoadingActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    // 로그인 창 띄우기
+    fun doLogin() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    // 로그인 결과 처리
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                startLoading()
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+            }
+        }
+    }
+    companion object {
+        private const val RC_SIGN_IN = 123
+    }
+}
