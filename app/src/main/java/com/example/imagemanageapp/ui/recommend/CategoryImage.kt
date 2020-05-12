cd72b3e0f0cca876e20e2228633944d0c49d96f7
rlaaldMINJI <elen6773@gmail.com>
2020-05-12 14:30:22 +0900

    update recommend ui

---
+++ app/src/main/java/com/example/imagemanageapp/ui/recommend/CategoryImage.kt
@@ -0,0 +1,8 @@
+package com.example.imagemanageapp.ui.recommend
+
+import java.util.ArrayList
+
+data class CategoryImage(
+    val token : String? = null,
+    val title : String? = null
+)
---
+++ app/src/main/java/com/example/imagemanageapp/ui/recommend/TitleData.kt
@@ -0,0 +1,8 @@
+package com.example.imagemanageapp.ui.recommend
+
+import java.util.ArrayList
+
+data class TitleData(
+
+    var list : ArrayList<String>
+)
